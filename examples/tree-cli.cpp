b54c0cfa29371c57c5123d5890efad4f6093b827
Beenv12 <36257286+Beenv12@users.noreply.github.com>
2018-04-13 09:32:05 +0300

    Update tree-cli.cpp

--- examples/tree-cli.cpp
+++ examples/tree-cli.cpp
@@ -2,18 +2,19 @@
 
 using namespace AVLTree;
 
-int main(int argc, char* argv[])
-{ 
-	TUI obj;    
-    	std::vector<int> a;
-    	int chosenValue ;
-	Tree tree = {3,4,2,6,5,7};
+int main(int argc, char *argv[])
+{
+	TUI obj;
+	std::vector<int> a;
+	int chosenValue;
+	//Tree tree = {3,4,2,6,5,7};
 
-    	for (int i=1; i<argc; i++)
-        	a.push_back(atoi(argv[i]));
+	for (int i = 1; i < argc; i++)
+		a.push_back(atoi(argv[i]));
 
-    	a=obj.CorrectFunction(a);
-        chosenValue=obj.ChosenFunction();
-	obj.MakeDecision(chosenValue);	
+	a = obj.CorrectFunction(a);
+	Tree tree(a);
+
+	chosenValue = obj.ChosenFunction();
+	obj.MakeDecisionTree(chosenValue, &tree);
 }
-
